b1fd969953fe46d38527f5ba30cbaebd5092b64d
Svoday Cheung <day961@gmail.com>
2013-03-19 21:28:21 +0800

    删除 calculater.pro

--- ui_widget.h
+++ ui_widget.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'widget.ui'
 **
-** Created: Tue Mar 19 21:03:43 2013
+** Created: Tue Mar 19 21:09:35 2013
 **      by: Qt User Interface Compiler version 4.8.1
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
